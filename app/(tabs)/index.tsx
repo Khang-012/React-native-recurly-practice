28cc591233c69b87a1f732af372125f67c5dcf93
Chusin616 <khahoang5746@gmail.com>
2026-06-09 19:40:10 +0700

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import "@/global.css";
-import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
@@ -9,26 +8,9 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5 ">
-      <Text className="text-2xl font-bold text-success">
+      <Text className="text-3xl font-sans-extrabold">
         Welcome to Nativewind!
       </Text>
-
-      <Link
-        href="/subscriptions/spotify"
-        className="mt-4 rounded-xl bg-primary text-white p-4"
-      >
-        Spotify Subscriptions
-      </Link>
-
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-        className="mt-4 rounded-xl bg-primary text-white p-4"
-      >
-        Claude Max
-      </Link>
     </SafeAreaView>
   );
 }
